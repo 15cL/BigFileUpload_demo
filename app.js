dea612033133010197b0d4cb41e2a7c09a5d61f0
Luo Shi Fa <1246449573@qq.com>
2023-08-23 23:04:15 +0800

    完成断点续传、秒传。

--- app.js
+++ app.js
@@ -38,45 +38,81 @@ const createFolder = function (folder) {
     fs.mkdirSync(folder);
   }
 };
+
 // 文件上传的路径
 var uploadFolder = "./uploads";
+// 创建文件夹
 createFolder(uploadFolder);
 createFolder("./uploads/temp");
+createFolder("./uploads/download");
 
 const upload = multer({ dest: "./uploads/temp" });
 
-// // 导入并注册用户路由模块
 app.post("/upload", upload.single("file"), (req, res) => {
-  console.log(req.file);
-  fs.renameSync(
-    req.file.path,
-    "./uploads/temp/" + `${req.body.context}=${req.body.index}`
-  );
+  // 重命名文件
+  fs.renameSync(req.file.path, "./uploads/temp/" + req.body.hash);
 
   return res.send({ status: 0, message: "上传成功" });
 });
 
 app.post("/finish", multipartMiddleware, (req, res) => {
-  const chunks = fs.readdirSync("./uploads/temp");
-  console.log(req.body);
-  console.log(chunks);
-  const filePath = path.join("./uploads", req.body.name);
-
+  // 读取temp文件夹中的分片，并返回数组
+  let chunks = [];
+  let arr = fs.readdirSync("./uploads/temp");
+  arr.forEach((v) => {
+    if (v.indexOf(req.body.fileHash) != -1) {
+      chunks.push(v);
+    }
+  });
+  console.log(chunks.length);
   //创建存储文件
+  const filePath = path.join(
+    "./uploads/download",
+    req.body.fileHash + req.body.name
+  );
   fs.writeFileSync(filePath, "");
 
-  if (chunks.length != req.body.chunks || chunks.length == 0) {
-    return res.send({ status: 0, message: "切片文件数量不符合" });
+  // 判断分片数目是否正确
+  if (chunks.length < req.body.chunks || chunks.length == 0) {
+    fs.unlinkSync("./uploads/download/" + req.body.fileHash + req.body.name);
+    return res.send({ status: 1, message: "切片文件数量不符合" });
   }
+
   for (let i = 0; i < chunks.length; i++) {
     // 将分片内容写入存储文件中
     fs.appendFileSync(
       filePath,
-      fs.readFileSync("./uploads/temp/" + `${req.body.context}=${i}`)
+      fs.readFileSync("./uploads/temp/" + `${req.body.fileHash}-${i}`)
     );
 
     // 删除已写入分片
-    fs.unlinkSync("./uploads/temp/" + `${req.body.context}=${i}`);
+    fs.unlinkSync("./uploads/temp/" + `${req.body.fileHash}-${i}`);
   }
-  return res.send({ status: 0, message: "收到请求结束命令" });
+  return res.send({ status: 0, message: "收到上传结束命令" });
+});
+
+// 秒传实现
+app.post("/verify", (req, res) => {
+  const files = fs.readdirSync("./uploads/download");
+  files.forEach((e) => {
+    if (e.indexOf(req.body.fileHash) != -1) {
+      return res.send({ status: 0, message: "上传成功" });
+    }
+  });
+  return res.send({ status: 1, message: "服务器查询不到该文件" });
+});
+
+// 恢复上传、继续上传检查已上传切片
+app.get("/already", (req, res) => {
+  const files = fs.readdirSync("./uploads/temp");
+  let arr = [];
+  files.forEach((e) => {
+    if (e.indexOf(req.query.fileHash) != -1) {
+      arr.push(e);
+    }
+  });
+  if (arr.length) {
+    return res.send({ status: 0, message: "查询到部分切片", data: arr });
+  }
+  return res.send({ status: 1, message: "查询不到切片" });
 });
